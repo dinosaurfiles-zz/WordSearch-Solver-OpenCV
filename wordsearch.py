d3666dadb88f881e00bde546386351c53712ea4d
dinosaurfiles <dinosaurfiles@users.noreply.github.com>
2017-05-08 01:01:24 +0800

    Found letters using hierarchy

--- wordsearch.py
+++ wordsearch.py
@@ -22,17 +22,17 @@ blur = cv2.GaussianBlur(gray, (5,5),0)
 thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
 # Find contours
-_, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+_, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
 # Set rectangles
-for cnt in reversed(contours):
+for i in range(len(contours)):
 	# Find rectangles that are the size of a character
-	# TODO: Remove contours that are inside a contour
-	if cv2.contourArea(cnt) > 50 and cv2.contourArea(cnt) < 250:
-		[x, y, w, h] = cv2.boundingRect(cnt)
-		cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+	if cv2.contourArea(contours[i]) > 50 and cv2.contourArea(contours[i]) < 250:
+		if hierarchy[0][i][3] == -1:
+			[x, y, w, h] = cv2.boundingRect(contours[i])
+			cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 1)
 		# TODO: Find nearest model using KNN
 
-cv2.imshow('Image', img)
-cv2.imshow('ImageBak', imgbak)
+cv2.imshow('Letter', img)
+cv2.imshow('Original Image', imgbak)
 cv2.waitKey(0)
